982023cd9642fe58b0e6a7270eed3563d99d66ee
DillonRanwala <dranwal@clemson.edu>
2023-03-14 00:19:06 -0400

    Try new bucket

--- cdk/log_storage/__init__.py
+++ cdk/log_storage/__init__.py
@@ -17,7 +17,7 @@ class LogStorage(core.Stack):
 
         
     def s3_log_bucket(self):
-        self.log_bucket = aws_s3.Bucket(self, 'quicksight-log-data',
+        self.log_bucket = aws_s3.Bucket(self, 'quicksight-log-data2',
                         block_public_access=aws_s3.BlockPublicAccess.BLOCK_ALL,
                         encryption=aws_s3.BucketEncryption.S3_MANAGED,
                         versioned=False,
