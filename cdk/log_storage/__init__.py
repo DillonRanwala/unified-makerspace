9be20b56ef79e8ab1bc5360cc7b7b3aeb9417996
DillonRanwala <dranwal@clemson.edu>
2023-03-14 21:57:57 -0400

    Trying test bucket with User and bucket policy again

--- cdk/log_storage/__init__.py
+++ cdk/log_storage/__init__.py
@@ -17,7 +17,7 @@ class LogStorage(core.Stack):
 
         
     def s3_log_bucket(self):
-        self.log_bucket = aws_s3.Bucket(self, 'quicksight-log-datatest', 
+        self.log_bucket = aws_s3.Bucket(self, 'quicksight-log-datanew', 
                         block_public_access=aws_s3.BlockPublicAccess.BLOCK_ALL,
                         encryption=aws_s3.BucketEncryption.S3_MANAGED,
                         versioned=False,
